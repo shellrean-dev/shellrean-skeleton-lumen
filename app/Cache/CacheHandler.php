6f76424c86cf42f8b87b74959f0d0cf383ecaae5
shellrean <wandinak17@gmail.com>
2021-05-07 05:54:22 +0700

    :sparkles: add: cache handler

---
+++ app/Cache/CacheHandler.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace ShellreanDev\Cache;
+
+/**
+ * @author shellrean <wandinak17@gmail.com>
+ * @year 2021
+ * ---------------------------------------
+ * Here we will handle and interact with cache
+ */
+use ShellreanDev\Cache\Interface\CacheHandlerInterface;
+
+class CacheHandler implements CacheHandlerInterface
+{
+    
+}
---
+++ app/Cache/Interface/CacheHandlerInterface.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace ShellreanDev\Cache\Interface;
+
+/**
+ * @author shellrean <wandinak17@gmail.com>
+ * @year 2021
+ * ---------------------------------------
+ * We will interact with cache
+ */
+interface CacheHandlerInterface
+{
+    
+}
